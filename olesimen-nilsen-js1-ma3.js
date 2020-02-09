8bb4a2b83bca9af3648d77d2d979a95efd09ba06
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-02-09 23:23:54 +0100

    Question 4

--- olesimen-nilsen-js1-ma3.js
+++ olesimen-nilsen-js1-ma3.js
@@ -32,3 +32,31 @@ function gamesLogger(json) {
 // Question 3
 let animalText = "These cats are outrageous.";
 animmalText = animalText.replace("cats", "giraffes");
+
+// Question 4
+/* Given the URL below, write code that checks if there is a userId parameter in the query string.
+
+If there is no userID parameter, redirect the browser to third.html.
+
+If there is a userID parameter and its value is less than 10, redirect to first.html.
+
+If there is a userID parameter and its value is 10 or greater, redirect to second.html. */
+
+// https://my.site.com?userId=7
+
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+
+let userId;
+
+if (params.has("userId")) {
+    userId = params.get("userId");
+
+    if (userId < 10) {
+        document.location.href = "first.html";
+    } else if (userId >= 10) {
+        document.location.href = "second.html";
+    }
+} else {
+    document.location.href = "third.html";
+}
