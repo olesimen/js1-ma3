168fd4868ca25f1eb27eab7bce1e9d81689cfeb2
Ole Simen Nilsen <olesimen.nilsen@gmail.com>
2020-02-09 21:39:22 +0100

    Question 2

--- olesimen-nilsen-js1-ma3.js
+++ olesimen-nilsen-js1-ma3.js
@@ -4,3 +4,34 @@
 } */
 
 const subtract = (a, b) => a - b;
+
+// Question 2
+/* Make a call to the URL below, pass the JSON it returns to a function and inside that function loop through the results and log each game's name.
+
+In the catch method of your code, redirect to error.html if there is an error. */
+
+const baseUrl = "https://api.rawg.io/api/";
+const gamesUrl = `${baseUrl}games?genres=`;
+
+let genres = "sports";
+const genreUrl = gamesUrl + genres;
+
+fetch(genreUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.dir(json);
+        gamesLogger(json);
+    })
+    .catch(function(error) {
+        console.dir(error);
+    });
+
+function gamesLogger(json) {
+    const games = json.results;
+
+    for (let i = 0; i < games.length; i++) {
+        console.log(games[i].name);
+    }
+}
